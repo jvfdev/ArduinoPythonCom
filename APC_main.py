46b55a7600b2121da3434295ee74f55ae733c31c
jvfdev <jvf.dev@gmail.com>
2018-05-26 18:32:33 -0500

    added radio button control for Arduino. Sends 0 or 1 string.

--- APC_main.py
+++ APC_main.py
@@ -1,1 +1,31 @@
-print('hello world')
+import serial
+from Tkinter import *
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+
+
+window = Tk()
+window.title("APC_GUI")
+window.geometry('350x200')
+
+lbl = Label(window, text="Use this tool to turn the Arduino ON or OFF", font=("arial",12,"bold"))
+
+lbl.grid(column=0, row=0,columnspan=2)
+
+def clicked1():
+    ser.write('0')
+
+
+def clicked2():
+    ser.write('1')
+
+
+rad1 = Radiobutton(window,text='OFF', value=1, command=clicked1)
+rad2 = Radiobutton(window,text='ON', value=2, command=clicked2)
+rad1.grid(column=0, row=1)
+rad2.grid(column=1, row=1)
+
+
+
+
+window.mainloop()
